0220cb78bce1ba4c4b91bfa88e9b02addff879f4
Mateusz Betlej <mateusz.betlej@photocentric.co.uk>
2022-01-19 13:42:45 +0000

    LC Nano customiser logo/cure image path

--- host/src/main/java/org/area515/resinprinter/services/CustomizerService.java
+++ host/src/main/java/org/area515/resinprinter/services/CustomizerService.java
@@ -209,7 +209,7 @@ public class CustomizerService {
 			// printer.showImage(img, true);
 			
 			try {
-				Process showingSlice = Runtime.getRuntime().exec(new String[]{"nice", "-n", "-2", "/opt/cwh/os/Linux/armv61/show_image", "-d", "5", "-t", "10", "-p", printerName, "-e", "10000", "-b", "5", "-x", "50000", "-m", "/home/pi/mask/mask.png", "/tmp/printdir/display_cure_to_be_hidden.zip/display_cure_to_be_hidden.png"});
+				Process showingSlice = Runtime.getRuntime().exec(new String[]{"nice", "-n", "-2", "/opt/cwh/os/Linux/armv61/show_image", "-d", "5", "-t", "10", "-p", printerName, "-e", "10000", "-b", "5", "-x", "50000", "-m", "/home/pi/mask/mask.png", "/home/pi/display_cure_to_be_hidden.png"});
 				showingSlice.waitFor();
 			} catch(Exception e) {
 				
@@ -227,9 +227,9 @@ public class CustomizerService {
 	@Path("projectCustomizerLogo/{customizerName}")
 	public void projectLogoImage(@PathParam("customizerName") String customizerName) throws SliceHandlingException, InappropriateDeviceException, NoPrinterFoundException {
 		Customizer customizer = customizersByName.getIfPresent(customizerName);
-		if (customizer == null) {
-			throw new IllegalArgumentException("Customizer is missing");
-		}
+		// if (customizer == null) {
+		// 	throw new IllegalArgumentException("Customizer is missing");
+		// }
 		
 		String printerName = customizer.getPrinterName();
 		if (printerName == null) {
@@ -253,7 +253,7 @@ public class CustomizerService {
 			// printer.showImage(img, true);
 			
 			try {
-				Process showingSlice = Runtime.getRuntime().exec(new String[]{"nice", "-n", "-2", "/opt/cwh/os/Linux/armv61/show_image", "-d", "5", "-t", "10", "-p", printerName, "-e", "15000", "-b", "5", "-x", "50000", "-m", "/home/pi/mask/mask.png", "/tmp/printdir/display_test_to_be_hidden.zip/display_test_to_be_hidden.png"});
+				Process showingSlice = Runtime.getRuntime().exec(new String[]{"nice", "-n", "-2", "/opt/cwh/os/Linux/armv61/show_image", "-d", "5", "-t", "10", "-p", printerName, "-e", "15000", "-b", "5", "-x", "50000", "-m", "/home/pi/mask/mask.png", "/home/pi/display_test_to_be_hidden.png"});
 				showingSlice.waitFor();
 			} catch(Exception e) {
 				
@@ -271,9 +271,9 @@ public class CustomizerService {
 	@Path("projectCustomizerOnPrinter/{customizerName}")
 	public void projectImage(@PathParam("customizerName") String customizerName) throws SliceHandlingException, InappropriateDeviceException, NoPrinterFoundException {
 		Customizer customizer = customizersByName.getIfPresent(customizerName);
-		if (customizer == null) {
-			throw new IllegalArgumentException("Customizer is missing");
-		}
+		// if (customizer == null) {
+		// 	throw new IllegalArgumentException("Customizer is missing");
+		// }
 		
 		String printerName = customizer.getPrinterName();
 		if (printerName == null) {
